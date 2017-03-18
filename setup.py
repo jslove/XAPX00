5c81f4d9154fb84562c4b41832fe68b1bb8ccd6e
Jay Love <jsloveiv@gmail.com>
2017-03-18 10:53:30 -0400

    Correct setup.py to read README.md

--- setup.py
+++ setup.py
@@ -9,21 +9,26 @@ sys.path.insert(0, '.')
 
 CURRENT_DIR = os.path.dirname(__file__)
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 # to deploy to pip, please use
 # make pythonpack
 # python setup.py register sdist upload
 # and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
+
 setup(name='XAPX00',
-      version='0.1.0',
+      version='0.1.1',
       description='Control an XAP800/XAP400 unit',
-      long_description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
+      long_description=open('README.md').read(),
       install_requires=['pyserial'],
       license='GNU',
-      keywords=['XAP800','XAPX00','XAP400','Genternet','ClearOne']
+      keywords=['XAP800', 'XAPX00', 'XAP400', 'Genternet', 'ClearOne'],
       maintainer='Jay Love',
       maintainer_email='jsloveiv@gmail.com',
       zip_safe=True,
       packages=find_packages(),
       include_package_data=True,
       url='https://github.com/jslove/XAPX00.git')
-      
+

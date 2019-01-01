18d25f0e82b2a0e1e15852c48982bdf4627251b5
Jay Love <jsloveiv@gmail.com>
2019-01-01 18:07:59 -0500

    Bumo version number, clean up README

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(fname):
 # and be sure to test it firstly using "python setup.py register sdist upload -r pypitest"
 
 setup(name='XAPX00',
-      version='0.2.4',
+      version='0.2.5',
       description='Control an XAP800/XAP400 unit',
       long_description=read('README.md'),
       install_requires=['pyserial>=3.3'],
